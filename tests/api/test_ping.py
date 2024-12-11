531f96ed521fc4a7508974e825ac150ff6903103
Romaniv1912 <romaniv1912@gmail.com>
2024-12-11 16:56:37 +0200

    set root_path in TestClient

--- tests/api/test_ping.py
+++ tests/api/test_ping.py
@@ -1,10 +1,8 @@
 from starlette.testclient import TestClient
 
-from src.core.conf import settings
-
 
 def test_ping(client: TestClient) -> None:
-    resp = client.get(f"{settings.APP.BASE_PATH}/ping")
+    resp = client.get('/ping')
 
     assert resp.status_code == 200, 'Bad status code'
     assert resp.json() == {'ping': 'pong'}, 'Response is not a pong response'
--- tests/conftest.py
+++ tests/conftest.py
@@ -5,9 +5,10 @@ import pytest
 from starlette.testclient import TestClient
 
 from main import app
+from src.core.conf import settings
 
 
 @pytest.fixture(scope='module')
 def client() -> Generator:
-    with TestClient(app) as c:
+    with TestClient(app, root_path=settings.APP.BASE_PATH) as c:
         yield c
