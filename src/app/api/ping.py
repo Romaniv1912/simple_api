60c8a8c0183e521833dfaae9e01cb70b224fe7ad
Romaniv1912 <romaniv1912@gmail.com>
2024-12-11 15:06:11 +0200

    refactor files

--- src/app/api/ping.py
+++ src/app/api/ping.py
@@ -5,8 +5,6 @@ from src.app.schema.ping import GetPing
 route = APIRouter()
 
 
-@route.get('/ping',
-           summary='Ping service',
-           description='Allow to ping service')
+@route.get('/ping', summary='Ping service', description='Allow to ping service')
 def ping() -> GetPing:
     return GetPing()
