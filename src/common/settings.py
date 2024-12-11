3e04ca4569cfe988f196d6a2ef0788a666960597
Romaniv1912 <romaniv1912@gmail.com>
2024-12-11 14:15:57 +0200

    add conf settings

---
+++ src/common/settings.py
@@ -0,0 +1,70 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class TokenSettings(BaseSettings):
+    """Token Settings"""
+
+    model_config = SettingsConfigDict(env_prefix='TOKEN_')
+
+    SECRET_KEY: str  # Key secrets.token_urlsafe(32)
+    ALGORITHM: str = 'HS256'  # algorithm
+    EXPIRE_SECONDS: int = 60 * 60 * 24 * 1  # expiration time unit seconds
+    PREFIX: str
+
+
+class AppSettings(BaseSettings):
+    """App Settings"""
+
+    model_config = SettingsConfigDict(env_prefix='APP_')
+
+    # FastAPI
+    TITLE: str
+    VERSION: str
+    DESCRIPTION: str
+    DOCS_URL: str | None = None
+    REDOCS_URL: str | None = None
+    OPENAPI_URL: str | None = None
+    BASE_PATH: str
+
+    PREFIX: str
+    SQLALCHEMY_ECHO: bool = False
+
+
+class DatabaseSettings(BaseSettings):
+    """Database Settings"""
+
+    model_config = SettingsConfigDict(env_prefix='POSTGRES_')
+
+    SQLALCHEMY_ECHO: bool = False
+    SCHEMA: str = 'postgresql+asyncpg'
+    HOST: str = 'localhost'
+    PORT: int = 5432
+    DB: str
+    USER: str
+    PASSWORD: str
+
+    @property
+    def url(self) -> str:
+        return f'{self.SCHEMA}://{self.USER}:{self.PASSWORD}@{self.HOST}:{self.PORT}/{self.DB}'
+
+
+class LogSettings(BaseSettings):
+    """Logs Settings"""
+
+    model_config = SettingsConfigDict(env_prefix='LOG_')
+
+    ROOT_LEVEL: str = 'NOTSET'
+    STD_FORMAT: str = (
+        '<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</> | <lvl>{level: <8}</> | '
+        '<cyan> {correlation_id} </> | <lvl>{message}</>'
+    )
+    LOGURU_FORMAT: str = (
+        '<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</> | <lvl>{level: <8}</> | '
+        '<cyan> {correlation_id} </> | <lvl>{message}</>'
+    )
+    CID_DEFAULT_VALUE: str = '-'
+    CID_UUID_LENGTH: int = 32  # must <= 32
+    STDOUT_LEVEL: str = 'INFO'
+    STDERR_LEVEL: str = 'ERROR'
+    STDOUT_FILENAME: str = 'api_access.log'
+    STDERR_FILENAME: str = 'api_error.log'
---
+++ src/core/conf.py
@@ -0,0 +1,43 @@
+import os
+
+from functools import lru_cache
+from os.path import join
+from pathlib import Path
+from typing import Literal
+
+from dotenv import load_dotenv
+from pydantic_settings import BaseSettings
+
+from src.common.settings import AppSettings, DatabaseSettings, LogSettings, TokenSettings
+
+# Get the project root directory
+# Or use an absolute path to the backend directory
+BASE_PATH = Path(__file__).resolve().parent.parent.parent
+
+# Log file path
+LOG_DIR = os.path.join(BASE_PATH, 'logs')
+
+load_dotenv(join(BASE_PATH, '.env.prod'))
+load_dotenv(join(BASE_PATH, '.env'), override=True)
+
+
+class Settings(BaseSettings):
+    """Global Settings"""
+
+    # Env Config
+    ENVIRONMENT: Literal['dev', 'pro']
+
+    APP: AppSettings = AppSettings()
+    TOKEN: TokenSettings = TokenSettings()
+    DB: DatabaseSettings = DatabaseSettings()
+    LOG: LogSettings = LogSettings()
+
+
+@lru_cache
+def get_settings() -> Settings:
+    """Get global configuration"""
+    return Settings()
+
+
+# Create configuration instance
+settings = get_settings()
