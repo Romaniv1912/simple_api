cd2de966c28dd8d20722c556e53b266c821b2e49
Romaniv1912 <romaniv1912@gmail.com>
2024-12-13 04:11:17 +0200

    add test env file

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,16 @@
+from os.path import join
 from typing import Generator
 
 import pytest
+from dotenv import load_dotenv
 
 from starlette.testclient import TestClient
 
 from main import app
-from src.core.conf import settings
+from src.core.conf import settings, BASE_PATH
+
+
+load_dotenv(join(BASE_PATH, '.env.test'), override=True)
 
 
 @pytest.fixture(scope='module')
